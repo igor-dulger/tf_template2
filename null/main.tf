98d63eef8a8af848a7cbca2a2a0f59e3df91606f
igor-dulger <igordulger@gmail.com>
2021-10-22 12:52:14 +0300

    Create main.tf

---
+++ null/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "resource1" {
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello big world!"
+    }
+  }
+  triggers = {
+    test = format("example+%s", timestamp())
+  }
+}
